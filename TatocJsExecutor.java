5bd4b6d5eb71a6b6a34f146a327717ffdb806db7
rahul8377072330 <40262405+rahul8377072330@users.noreply.github.com>
2018-06-27 15:47:13 +0530

    Add files via upload

--- TatocJsExecutor.java
+++ TatocJsExecutor.java
@@ -7,78 +7,94 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class TatocJsExecutor {
+public class TatocJsExecutor{
 
-	public static void main(String[] arg) throws InterruptedException{
 
-		WebDriver driver=new ChromeDriver();
-
-		JavascriptExecutor js= (JavascriptExecutor)driver; 
+	WebDriver driver = new ChromeDriver();
+	JavascriptExecutor js= (JavascriptExecutor)driver;
+	public String getCurrentLink() {
+		return driver.getCurrentUrl();
+	}
+	
+	public void openBrowserAndChooseBasic() {
 		driver.get("http://10.0.1.86/tatoc");
-
 		WebElement BasicCourse = driver.findElement(By.linkText("Basic Course"));
 		js.executeScript("arguments[0].click();", BasicCourse);
-
+		}
+	
+	public void greenBox() {
 
 		WebElement greenBox = driver.findElement(By.className("greenbox"));
 		js.executeScript("arguments[0].click();", greenBox);
 
-		driver.switchTo().frame("main");
-		String box1 = driver.findElement(By.id("answer")).getAttribute("class");
-		
-		WebElement proceed=driver.findElement(By.linkText("Proceed"));
-		while(true){
-				WebElement repaintBox=driver.findElement(By.linkText("Repaint Box 2"));
-				js.executeScript("arguments[0].click();", repaintBox);
-				driver.switchTo().frame("child"); 
-				String box2 = driver.findElement(By.id("answer")).getAttribute("class");
-				driver.switchTo().parentFrame(); 
+		}
+	public void frameDungeon() {
+	driver.switchTo().frame("main");
+	String box1 = driver.findElement(By.id("answer")).getAttribute("class");
+	
+	WebElement proceed=driver.findElement(By.linkText("Proceed"));
+	while(true){
+			WebElement repaintBox=driver.findElement(By.linkText("Repaint Box 2"));
+			js.executeScript("arguments[0].click();", repaintBox);
+			driver.switchTo().frame("child"); 
+			String box2 = driver.findElement(By.id("answer")).getAttribute("class");
+			driver.switchTo().parentFrame(); 
 
-				if(box1.equals(box2))
-				{
-					js.executeScript("arguments[0].click();", proceed);
-					break;
-				}
-		     }	
-				driver.switchTo().defaultContent();
-				
-				Actions act=new Actions(driver);
-				
-				WebElement dg=driver.findElement(By.id("dragbox"));
-				WebElement dp=driver.findElement(By.id("dropbox"));
-				act.dragAndDrop(dg, dp).build().perform();
-				 
-				 WebElement drag_proceed = driver.findElement(By.linkText("Proceed"));
-				 js.executeScript("arguments[0].click();",drag_proceed);
-				 
-				 WebElement popup =  driver.findElement(By.linkText("Launch Popup Window"));
-				 js.executeScript("arguments[0].click();",popup);
-				 
-				 ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
-                 driver.switchTo().window(tabs2.get(1));
-                 
-                 WebElement sc = driver.findElement(By.cssSelector("#name"));
-                 js.executeScript("document.getElementById('name').value='hello world';");
-                 
-                 WebElement sub = driver.findElement(By.cssSelector("#submit"));
-                 js.executeScript("arguments[0].click();",sub);
-                 driver.switchTo().window(tabs2.get(0));
-                 
-                 WebElement pro = driver.findElement(By.linkText("Proceed"));
-                 js.executeScript("arguments[0].click();",pro);
-                 
-                 WebElement token = driver.findElement(By.linkText("Generate Token"));
-                 js.executeScript("arguments[0].click();",token);
-                 
-                 String Token = driver.findElement(By.id("token")).getText();
-                 String[] splited = Token.split("\\s+"); 
-                 Cookie token1 = new Cookie("Token", splited[1]);
-                 driver.manage().addCookie(token1);
-                 WebElement tok = driver.findElement(By.linkText("Proceed"));
-                 js.executeScript("arguments[0].click();",tok);
-                 
-                 Thread.sleep(1000);
-                 driver.close();	
+			if(box1.equals(box2))
+			{
+				js.executeScript("arguments[0].click();", proceed);
+				break;
+			}
+	     }
+	driver.switchTo().defaultContent();
 	}
+
+	public void dragAndDrop() {
+		Actions act=new Actions(driver);
+		
+		WebElement dg=driver.findElement(By.id("dragbox"));
+		WebElement dp=driver.findElement(By.id("dropbox"));
+		act.dragAndDrop(dg, dp).build().perform();
+		 
+		 WebElement drag_proceed = driver.findElement(By.linkText("Proceed"));
+		 js.executeScript("arguments[0].click();",drag_proceed);
+	}
+	
+	public void popUpWindow() {
+		
+	 WebElement popup =  driver.findElement(By.linkText("Launch Popup Window"));
+	 js.executeScript("arguments[0].click();",popup);
+	 
+	 ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
+     driver.switchTo().window(tabs2.get(1));
+     
+     WebElement sc = driver.findElement(By.cssSelector("#name"));
+     js.executeScript("document.getElementById('name').value='hello world';");
+     
+     WebElement sub = driver.findElement(By.cssSelector("#submit"));
+     js.executeScript("arguments[0].click();",sub);
+     driver.switchTo().window(tabs2.get(0));
+     
+     WebElement pro = driver.findElement(By.linkText("Proceed"));
+     js.executeScript("arguments[0].click();",pro);
+	}
+	
+	public void cookie() {
+		
+		 WebElement token = driver.findElement(By.linkText("Generate Token"));
+         js.executeScript("arguments[0].click();",token);
+         
+         String Token = driver.findElement(By.id("token")).getText();
+         String[] splited = Token.split("\\s+"); 
+         Cookie token1 = new Cookie("Token", splited[1]);
+         driver.manage().addCookie(token1);
+         WebElement tok = driver.findElement(By.linkText("Proceed"));
+         js.executeScript("arguments[0].click();",tok);
+       
+	}  
+	 
+	
 }
---
+++ TestTatoc.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestTatoc {
+
+
+	TatocJsExecutor tat =new TatocJsExecutor();
+	
+	@Test(priority = 0)
+	public void testOpenBrowserAndChooseBasic() {
+		tat.openBrowserAndChooseBasic();
+		Assert.assertTrue(tat.getCurrentLink().contains("grid/gate"));
+	}
+	
+	@Test(priority = 1)
+	public void testGreenBox() {
+		tat.greenBox();
+		Assert.assertTrue(tat.getCurrentLink().contains("frame/dungeon"));
+		
+	}
+	
+	@Test(priority = 2)
+	public void testFrameDungeon() {
+		tat.frameDungeon();
+		Assert.assertTrue(tat.getCurrentLink().contains("basic/drag"));
+	}
+	@Test(priority = 3)
+	public void testDragAndDrop() {
+		tat.dragAndDrop();
+		Assert.assertTrue(tat.getCurrentLink().contains("basic/windows"));
+	}
+	
+	@Test(priority = 4)
+	public void testPopUpWindow() {
+		tat.popUpWindow();
+		Assert.assertTrue(tat.getCurrentLink().contains("basic/cookie"));
+	}
+	
+	@Test(priority = 5)
+	public void testCookie() {
+		tat.cookie();
+		Assert.assertTrue(tat.getCurrentLink().contains("tatoc/end"));
+	}
+
+}
